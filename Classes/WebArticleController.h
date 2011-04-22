6ff858cdd40215424e7a376e03a187d1c18b7e22
johnwarde@gmail.com <johnwarde@gmail.com@69d5df2f-4347-c3ce-6a0b-8f2bb7890814>
2011-04-22 13:22:36 +0000

    Fixed Issue 5:	Disable/Grey out button after user clicks "Save" button: When user clicks on the Save button on a web article, the Save button disappears to indicate it has been saved to Save Articles

--- Classes/WebArticleController.h
+++ Classes/WebArticleController.h
@@ -12,22 +12,25 @@
 @interface WebArticleController : UIViewController {
 	NSIndexPath *index;
 	
-	IBOutlet UILabel *titleLabel;
+	IBOutlet UILabel	*titleLabel;
 	IBOutlet UITextView *descriptionLabel;
-	IBOutlet UILabel *linkLabel;
-	IBOutlet UILabel *dateAndAuthorLabel;
-	IBOutlet UILabel *sourceLabel;
-	IBOutlet UILabel *categoryLabel;
-	IBOutlet UILabel *commentsLabel;
+	IBOutlet UILabel	*linkLabel;
+	IBOutlet UILabel	*dateAndAuthorLabel;
+	IBOutlet UILabel	*sourceLabel;
+	IBOutlet UILabel	*categoryLabel;
+	IBOutlet UILabel	*commentsLabel;
+	
+	IBOutlet UIButton *saveButton; // so that we can change the state (enable/disable)
 }
 
-@property(nonatomic, retain) IBOutlet UILabel *titleLabel;
-@property(nonatomic, retain) IBOutlet UITextView *descriptionLabel;
-@property(nonatomic, retain) IBOutlet UILabel *linkLabel;
-@property(nonatomic, retain) IBOutlet UILabel *dateAndAuthorLabel;
-@property(nonatomic, retain) IBOutlet UILabel *sourceLabel;
-@property(nonatomic, retain) IBOutlet UILabel *categoryLabel;
-@property(nonatomic, retain) IBOutlet UILabel *commentsLabel;
+@property(nonatomic, retain) IBOutlet UILabel		*titleLabel;
+@property(nonatomic, retain) IBOutlet UITextView	*descriptionLabel;
+@property(nonatomic, retain) IBOutlet UILabel		*linkLabel;
+@property(nonatomic, retain) IBOutlet UILabel		*dateAndAuthorLabel;
+@property(nonatomic, retain) IBOutlet UILabel		*sourceLabel;
+@property(nonatomic, retain) IBOutlet UILabel		*categoryLabel;
+@property(nonatomic, retain) IBOutlet UILabel		*commentsLabel;
+@property(nonatomic, retain) IBOutlet UIButton		*saveButton;
 
 -(id)initWithIndexPath: (NSIndexPath *)indexPath;
 -(IBAction) saveArticleClicked:(id) sender;
